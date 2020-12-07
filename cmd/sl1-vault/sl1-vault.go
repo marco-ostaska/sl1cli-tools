f58e8340f3ca2b607e2ede057793d538ea136985
marco-ostaska <34462124+marco-ostaska@users.noreply.github.com>
2020-12-07 14:00:45 -0300

    Create sl1-vault.go

---
+++ cmd/sl1-vault/sl1-vault.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/marco-ostaska/sl1cli-tools/internal/pkg/apicryptcfg"
+)
+
+var usr apicryptcfg.UserInfo
+
+func (fl *flArgs) newCFG() error {
+	return usr.SetInfo(*fl.username, *fl.passwd, *fl.url)
+}
+
+func (fl *flArgs) updateCFG() error {
+	err := usr.ReadCryptFile()
+	if err != nil {
+		eStr := fmt.Sprintf("%v", err)
+		if strings.Contains(eStr, "no such file or directory") {
+			fmt.Println("No credentials found, please try create a new credential vault first")
+			return err
+		}
+		fmt.Println(err)
+		return err
+	}
+
+	fmt.Println("Updating credentials for", usr.URL, "user", usr.UserAPI)
+	return usr.SetInfo(*fl.username, *fl.passwd, usr.URL)
+}
+
+func main() {
+	var fl flArgs
+	err := fl.initFlag()
+	if err != nil {
+		return
+	}
+}
